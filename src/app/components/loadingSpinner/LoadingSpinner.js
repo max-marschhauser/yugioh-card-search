c4b2157f825ebeab0b28910bc44a900f62cf0134
max-marschhauser <105870867+max-marschhauser@users.noreply.github.com>
2022-10-05 11:17:56 +0200

    changes to loading spinner

--- src/app/components/loadingSpinner/LoadingSpinner.js
+++ src/app/components/loadingSpinner/LoadingSpinner.js
@@ -3,13 +3,11 @@ import "./loadingSpinner.scss";
 
 export default function Loading() {
 	return (
-		<div className="container--loading">
-			<div className="spinner">
-				Loading...
-				<div className="spinner__sector spinner__sector--red"></div>
-				<div className="spinner__sector spinner__sector--green"></div>
-				<div className="spinner__sector spinner__sector--blue"></div>
-			</div>
+		<div className="spinner">
+			Loading...
+			<div className="spinner__sector spinner__sector--red"></div>
+			<div className="spinner__sector spinner__sector--green"></div>
+			<div className="spinner__sector spinner__sector--blue"></div>
 		</div>
 	);
 }
