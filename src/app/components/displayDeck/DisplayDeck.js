d66bd35cc25a9d2f168fc531c33c03799ba266f8
max-marschhauser <max.marschhauser@gmail.com>
2022-10-09 16:29:40 +0200

    small changes

--- src/app/components/displayDeck/DisplayDeck.js
+++ src/app/components/displayDeck/DisplayDeck.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 export default function DisplayDeck(selectedCards) {
+	// displayDeck bi trebao biti sličan kao i displayCards, druga varijabla koja se šalje kao search će biti array s id-evima karata u deku pa će map ići kroz njih i prikazati samo karte iz arraya
+
 	return (
 		<>
 			{selectedCards.map((card) => (
