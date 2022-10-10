e0b8571e4ab8bb9cbb8c68ae76364ddd45f12957
max-marschhauser <105870867+max-marschhauser@users.noreply.github.com>
2022-10-10 12:07:50 +0200

    displaying cards on deck page

--- src/app/components/displayDeck/DisplayDeck.js
+++ src/app/components/displayDeck/DisplayDeck.js
@@ -1,13 +1,81 @@
-import React from "react";
+// items su sve karte sa url linka, a selectedCards je array s id-ovima karata u deku
 
-export default function DisplayDeck(selectedCards) {
-	// displayDeck bi trebao biti sličan kao i displayCards, druga varijabla koja se šalje kao search će biti array s id-evima karata u deku pa će map ići kroz njih i prikazati samo karte iz arraya
+import "./displayDeck.scss";
+
+export default function DisplayDeck({ items, selectedCards }) {
+	console.log(selectedCards);
 
 	return (
 		<>
-			{selectedCards.map((card) => (
-				<div key={card}>{card}</div>
-			))}
+			{items.map((card) => {
+				if (selectedCards.includes(card.id)) {
+					return (
+						<div
+							key={card.id}
+							className={
+								card.type === "Spell Card"
+									? "card spellCard"
+									: card.type === "Trap Card"
+									? "card trapCard"
+									: card.type === "Normal Monster"
+									? "card monsterCardNormal"
+									: card.type === "Ritual Monster" || card.type === "Ritual Effect Monster"
+									? "card monsterCardRitual"
+									: card.type === "Fusion Monster"
+									? "card monsterCardFusion"
+									: card.type === "Effect Monster" ||
+									  card.type === "Flip Effect Monster" ||
+									  card.type === "Gemini Monster" ||
+									  card.type === "Spirit Monster" ||
+									  card.type === "Toon Monster" ||
+									  card.type === "Union Effect Monster"
+									? "card monsterCardEffect"
+									: "card"
+							}>
+							<header className="card__title" data-id={card.id}>
+								{JSON.stringify(card.name)}
+							</header>
+							<p className="card__type">{JSON.stringify(card.type)}</p>
+							<p className="card__item">
+								<b>Type: </b>
+								{JSON.stringify(card.race)}
+							</p>
+							{JSON.stringify(card.type).includes("Monster") ? (
+								<>
+									<p className="card__item">
+										<b>Attribute: </b>
+										{JSON.stringify(card.attribute)}
+									</p>
+									<p className="card__item">
+										<b>Level: </b>
+										{JSON.stringify(card.level)}
+										&nbsp;
+										<img
+											className="monsterLevelImage"
+											src="monsterLevelImg.png"
+											alt="monster card level"
+										/>
+									</p>
+									<p className="card__item">
+										<b>ATK: </b>
+										{JSON.stringify(card.atk)}
+										<b>/ DEF: </b>
+										{JSON.stringify(card.def)}
+									</p>
+								</>
+							) : (
+								<></>
+							)}
+							<p className="card__item card__item__cardText">
+								<b>Card text: </b>
+								{JSON.stringify(card.desc)}
+							</p>
+						</div>
+					);
+				} else {
+					return null;
+				}
+			})}
 		</>
 	);
 }
--- src/app/pages/deck/Deck.js
+++ src/app/pages/deck/Deck.js
@@ -1,14 +1,35 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import DisplayDeck from "../../components/displayDeck/DisplayDeck";
 import "./deck.scss";
+import LoadingSpinner from "../../components/loadingSpinner/LoadingSpinner";
 
 export default function Deck() {
 	const LOCAL_STORAGE_KEY = "myDeck";
 	let myDeck = localStorage.getItem(LOCAL_STORAGE_KEY);
 	let selectedCards = [];
+	const [loading, setLoading] = useState(true);
+
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		setLoading(true);
+		axios
+			.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php?startdate=01/01/2001&enddate=1/1/2008&misc=yes`)
+			.then((response) => {
+				setLoading(false);
+				setItems(response.data.data);
+			})
+			.catch(() => {
+				alert(
+					"There is no such card in database, change your search options. Please notice that this version of application only includes TCG Yu-Gi-Oh cards released until 1. 1. 2008. We are planing to add more cards in newer versions of app."
+				);
+			});
+	}, []);
 
 	if (myDeck === null) {
-		alert("Add cards to deck");
+		selectedCards = [];
 	} else if (myDeck.length === 8) {
 		selectedCards[0] = parseInt(myDeck);
 	} else {
@@ -18,9 +39,15 @@ export default function Deck() {
 		}
 	}
 
+	console.log("selected cards are: ");
+	console.log(selectedCards);
+
 	return (
 		<>
-			<div className="container--page">{DisplayDeck(selectedCards)}</div>;
+			<div className="container--deck">
+				{loading ? <LoadingSpinner /> : <DisplayDeck items={items} selectedCards={selectedCards} />}
+			</div>
+			;
 		</>
 	);
 }
