eb45ce3f22b4f36fa4c481c25e98c4589f78a8b8
max-marschhauser <max.marschhauser@gmail.com>
2022-10-11 18:32:39 +0200

    filter component

--- src/app/components/filterForm/FilterForm.js
+++ src/app/components/filterForm/FilterForm.js
@@ -1,12 +1,132 @@
 import React from "react";
+import changeSearchType from "../../utils/changeSearchType";
 
-export default function FilterForm() {
-	return <></>;
-}
-import React from 'react'
-
-export default function FilterForm() {
-  return (
-	<div>FilterForm</div>
-  )
+export default function FilterForm({ searchChanger, typeChanger, sortChanger }) {
+	return (
+		<form className="container--filter">
+			<div className="container--filter--column">
+				<label className="noPointer" htmlFor="searchWord">
+					Name / Card effect / Card type:
+				</label>
+				<input
+					type="text"
+					id="searchWord"
+					name="searchWord"
+					placeholder="search cards"
+					onChange={(e) => searchChanger(e.target.value)}
+				/>
+			</div>
+			<fieldset>
+				<legend>Search for:</legend>
+				<div className="container--filter--fieldset">
+					<div>
+						<input
+							type="checkbox"
+							id="normalMonster"
+							name="type"
+							value="Normal Monster"
+							onChange={(e) => typeChanger(changeSearchType(e.target.value))}
+						/>
+						<label htmlFor="normalMonster">Normal Monster</label>
+					</div>
+					<div>
+						<input
+							type="checkbox"
+							id="effectMonster"
+							name="type"
+							value="Effect Monster,Flip Effect Monster,Gemini Monster,Spirit Monster,Toon Monster,Union Effect Monster"
+							onChange={(e) => typeChanger(changeSearchType(e.target.value))}
+						/>
+						<label htmlFor="effectMonster">Effect Monster</label>
+					</div>
+					<div>
+						<input
+							type="checkbox"
+							id="ritualMonster"
+							name="type"
+							value="Ritual Effect Monster,Ritual Monster"
+							onChange={(e) => typeChanger(changeSearchType(e.target.value))}
+						/>
+						<label htmlFor="ritualMonster">Ritual Monster</label>
+					</div>
+					<div>
+						<input
+							type="checkbox"
+							id="fusionMonster"
+							name="type"
+							value="Fusion Monster"
+							onChange={(e) => typeChanger(changeSearchType(e.target.value))}
+						/>
+						<label htmlFor="fusionMonster">Fusion Monster</label>
+					</div>
+					<div>
+						<input
+							type="checkbox"
+							id="spell"
+							name="type"
+							value="Spell Card"
+							onChange={(e) => typeChanger(changeSearchType(e.target.value))}
+						/>
+						<label htmlFor="spell">Spell</label>
+					</div>
+					<div>
+						<input
+							type="checkbox"
+							id="trap"
+							name="type"
+							value="Trap Card"
+							onChange={(e) => typeChanger(changeSearchType(e.target.value))}
+						/>
+						<label htmlFor="trap">Trap</label>
+					</div>
+				</div>
+			</fieldset>
+			<fieldset>
+				<legend>Sort cards by:</legend>
+				<div className="container--filter--fieldset">
+					<div>
+						<input
+							type="radio"
+							id="name"
+							name="sort"
+							value="name"
+							onChange={(e) => sortChanger(e.target.value)}
+							defaultChecked
+						/>
+						<label htmlFor="name">Name</label>
+					</div>
+					<div>
+						<input
+							type="radio"
+							id="atk"
+							name="sort"
+							value="atk"
+							onChange={(e) => sortChanger(e.target.value)}
+						/>
+						<label htmlFor="atk">Atk</label>
+					</div>
+					<div>
+						<input
+							type="radio"
+							id="def"
+							name="sort"
+							value="def"
+							onChange={(e) => sortChanger(e.target.value)}
+						/>
+						<label htmlFor="def">Def</label>
+					</div>
+					<div>
+						<input
+							type="radio"
+							id="level"
+							name="sort"
+							value="level"
+							onChange={(e) => sortChanger(e.target.value)}
+						/>
+						<label htmlFor="level">Level</label>
+					</div>
+				</div>
+			</fieldset>
+		</form>
+	);
 }
--- src/app/pages/search/Search.js
+++ src/app/pages/search/Search.js
@@ -3,8 +3,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import LoadingSpinner from "../../components/loadingSpinner/LoadingSpinner";
 import DisplayCards from "../../components/displayCards/DisplayCards";
+import FilterForm from "../../components/filterForm/FilterForm";
 import "./search.scss";
-import changeSearchType from "../../utils/changeSearchType";
 
 export default function Search() {
 	const [items, setItems] = useState([]);
@@ -34,131 +34,7 @@ export default function Search() {
 
 	return (
 		<>
-			<form className="container--filter">
-				<div className="container--filter--column">
-					<label className="noPointer" htmlFor="searchWord">
-						Name / Card effect / Card type:
-					</label>
-					<input
-						type="text"
-						id="searchWord"
-						name="searchWord"
-						placeholder="search cards"
-						onChange={(e) => setSearchWord(e.target.value)}
-					/>
-				</div>
-				<fieldset>
-					<legend>Search for:</legend>
-					<div className="container--filter--fieldset">
-						<div>
-							<input
-								type="checkbox"
-								id="normalMonster"
-								name="type"
-								value="Normal Monster"
-								onChange={(e) => setType(changeSearchType(e.target.value))}
-							/>
-							<label htmlFor="normalMonster">Normal Monster</label>
-						</div>
-						<div>
-							<input
-								type="checkbox"
-								id="effectMonster"
-								name="type"
-								value="Effect Monster,Flip Effect Monster,Gemini Monster,Spirit Monster,Toon Monster,Union Effect Monster"
-								onChange={(e) => setType(changeSearchType(e.target.value))}
-							/>
-							<label htmlFor="effectMonster">Effect Monster</label>
-						</div>
-						<div>
-							<input
-								type="checkbox"
-								id="ritualMonster"
-								name="type"
-								value="Ritual Effect Monster,Ritual Monster"
-								onChange={(e) => setType(changeSearchType(e.target.value))}
-							/>
-							<label htmlFor="ritualMonster">Ritual Monster</label>
-						</div>
-						<div>
-							<input
-								type="checkbox"
-								id="fusionMonster"
-								name="type"
-								value="Fusion Monster"
-								onChange={(e) => setType(changeSearchType(e.target.value))}
-							/>
-							<label htmlFor="fusionMonster">Fusion Monster</label>
-						</div>
-						<div>
-							<input
-								type="checkbox"
-								id="spell"
-								name="type"
-								value="Spell Card"
-								onChange={(e) => setType(changeSearchType(e.target.value))}
-							/>
-							<label htmlFor="spell">Spell</label>
-						</div>
-						<div>
-							<input
-								type="checkbox"
-								id="trap"
-								name="type"
-								value="Trap Card"
-								onChange={(e) => setType(changeSearchType(e.target.value))}
-							/>
-							<label htmlFor="trap">Trap</label>
-						</div>
-					</div>
-				</fieldset>
-				<fieldset>
-					<legend>Sort cards by:</legend>
-					<div className="container--filter--fieldset">
-						<div>
-							<input
-								type="radio"
-								id="name"
-								name="sort"
-								value="name"
-								onChange={(e) => setSort(e.target.value)}
-								defaultChecked
-							/>
-							<label htmlFor="name">Name</label>
-						</div>
-						<div>
-							<input
-								type="radio"
-								id="atk"
-								name="sort"
-								value="atk"
-								onChange={(e) => setSort(e.target.value)}
-							/>
-							<label htmlFor="atk">Atk</label>
-						</div>
-						<div>
-							<input
-								type="radio"
-								id="def"
-								name="sort"
-								value="def"
-								onChange={(e) => setSort(e.target.value)}
-							/>
-							<label htmlFor="def">Def</label>
-						</div>
-						<div>
-							<input
-								type="radio"
-								id="level"
-								name="sort"
-								value="level"
-								onChange={(e) => setSort(e.target.value)}
-							/>
-							<label htmlFor="level">Level</label>
-						</div>
-					</div>
-				</fieldset>
-			</form>
+			<FilterForm searchChanger={setSearchWord} typeChanger={setType} sortChanger={setSort} />
 			<div className="container--search">
 				{loading ? <LoadingSpinner /> : <DisplayCards items={items} searchWord={searchWord} />}
 			</div>
