5511dd6899ef8c4953cee3223d28bc6b2c056c39
max-marschhauser <max.marschhauser@gmail.com>
2022-10-12 18:19:00 +0200

    started making cart page

--- src/app/pages/cart/Cart.js
+++ src/app/pages/cart/Cart.js
@@ -1,61 +1,20 @@
-/*
 import React from "react";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import DisplayDeck from "../../components/displayDeck/DisplayDeck";
 import "./cart.scss";
-import LoadingSpinner from "../../components/loadingSpinner/LoadingSpinner";
-
-export default function Deck() {
-	const LOCAL_STORAGE_KEY = "myDeck";
-	let myDeck = localStorage.getItem(LOCAL_STORAGE_KEY);
-	let selectedCards = [];
-	const [loading, setLoading] = useState(true);
-
-	const [items, setItems] = useState([]);
-
-	useEffect(() => {
-		setLoading(true);
-		axios
-			.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php?startdate=01/01/2001&enddate=1/1/2008&misc=yes`)
-			.then((response) => {
-				setLoading(false);
-				setItems(response.data.data);
-			})
-			.catch(() => {
-				alert(
-					"There is no such card in database, change your search options. Please notice that this version of application only includes TCG Yu-Gi-Oh cards released until 1. 1. 2008. We are planing to add more cards in newer versions of app."
-				);
-			});
-	}, []);
-
-	if (myDeck === null) {
-		selectedCards = [];
-	} else if (myDeck.length === 8) {
-		selectedCards[0] = parseInt(myDeck);
-	} else {
-		selectedCards = myDeck.split(",");
-		for (let i = 0; i < selectedCards.length; i++) {
-			selectedCards[i] = parseInt(selectedCards[i]);
-		}
-	}
-
-	console.log("selected cards are: ");
-	console.log(selectedCards);
-
-	return (
-		<>
-			<div className="container--deck">
-				{loading ? <LoadingSpinner /> : <DisplayDeck items={items} selectedCards={selectedCards} />}
-			</div>
-			;
-		</>
-	);
-}
-*/
-
-import React from "react";
 
 export default function Cart() {
+	const LOCAL_STORAGE_KEY = "Cart";
+	let storage = localStorage.getItem(LOCAL_STORAGE_KEY);
+	let cart = [];
+
+	if (storage === null) {
+		cart = [];
+	} else if (storage.length === 8) {
+		cart[0] = storage;
+	} else {
+		cart = storage.split(",");
+	}
+
+	console.log(cart);
+
 	return <div></div>;
 }
