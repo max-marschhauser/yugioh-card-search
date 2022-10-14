970ebf60eabf544df362913004057ebeb02873a1
max-marschhauser <max.marschhauser@gmail.com>
2022-10-14 18:17:41 +0200

    updating remove from cart function

--- src/app/utils/removeCardFromCart.js
+++ src/app/utils/removeCardFromCart.js
@@ -1,26 +1,25 @@
-export default function addCardToCart(card) {
+import { useState } from "react";
+
+export default function RemoveCardToCart(card) {
 	let parent = card.target.parentElement;
 	let name = parent.querySelector("[data-id]");
 	let cardId = name.dataset.id;
 	const LOCAL_STORAGE_KEY = "Cart";
 	let storage = localStorage.getItem(LOCAL_STORAGE_KEY);
-	let cart = [];
+
+	let [cart, setCart] = useState([]);
 
 	if (storage.length === 8) {
-		cart[0] = storage;
+		setCart(storage);
 	} else {
-		cart = storage.split(",");
+		setCart(storage.split(","));
 	}
 
-	console.log(cart);
-	console.log(cardId);
-
-	cart = cart.filter(function (item) {
-		return item !== cardId;
-	});
-
-	console.log(cart);
-	console.log(cardId);
+	setCart(
+		cart.filter(function (item) {
+			return item !== cardId;
+		})
+	);
 
 	localStorage.setItem(LOCAL_STORAGE_KEY, cart);
 }
